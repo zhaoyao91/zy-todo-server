82e514fdc5d3ac92b7017bbbec8a21cc7280f578
zhaoyao91 <zhaoyao91@163.com>
2016-08-29 11:39:11 +0800

    support cors

--- app/app.js
+++ app/app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
+const cors = require('cors');
 const routes = require('./routes');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
